3ac7733d9bd7106798f716d6eb8f5dff4afd7930
tonmoy <tonmoy3673@gmail.com>
2024-11-24 21:52:11 +0600

    :truck: set routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Components/Route/router'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <div className='max-w-5xl mx-auto'>
+   <RouterProvider router={router}/>
+   </div>
   </StrictMode>,
 )
