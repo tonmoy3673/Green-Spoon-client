cf9c16a0168be17e6e8c9e01234c759a66228399
tonmoy <tonmoy3673@gmail.com>
2024-11-25 21:54:17 +0600

    :books: added images

--- src/Components/Pages/Home/Banner.jsx
+++ src/Components/Pages/Home/Banner.jsx
@@ -1,7 +1,22 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Banner =()=>{
     return (
         <div>
-
+             <Carousel>
+                <div>
+                    <img src="assets/1.jpeg" />
+                    
+                </div>
+                <div>
+                    <img src="assets/2.jpeg" />
+                    
+                </div>
+                <div>
+                    <img src="assets/3.jpeg" />
+                    
+                </div>
+            </Carousel>
         </div>
     )
 };
