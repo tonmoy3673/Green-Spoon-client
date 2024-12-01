9e36c59882347e020f3604a649f3a143fa33fde4
tonmoy <tonmoy3673@gmail.com>
2024-12-01 21:24:15 +0600

    :bulb: create title page

---
+++ src/assets/images/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,11 @@
+const SectionTitle =({title,subTitle})=>{
+    return (
+        <div>
+            <p>{subTitle}</p>
+            <h2>{title}</h2>
+        </div>
+
+    )
+};
+
+export default SectionTitle;
