3edb334012fd6bb69e2ba2cf9cc52a251b683166
tonmoy <tonmoy3673@gmail.com>
2024-12-07 23:17:55 +0600

    :bulb: added bg images

--- src/Components/Pages/Home/AllMenu.jsx
+++ src/Components/Pages/Home/AllMenu.jsx
@@ -5,12 +5,14 @@ import Menu from "./Menu";
 
 const AllMenu =()=>{
     const [menu,setMenu]=useState([]);
-    const [display,setDisplay]= useState([4]);
+    const [display,setDisplay]= useState(4);
     fetch('/data.json')
     .then((res)=>res.json())
     .then((data)=>setMenu(data))
 
-    
+    // useEffect(()=>{
+    //     setMenu(display)
+    // },[display])
 
     return(
         <div className="mb-6 md:mb-16">
@@ -21,16 +23,18 @@ const AllMenu =()=>{
             {/* ============= Menu Section ========== */}
                 <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10 md:gap-16">
                     {
-                    menu && menu.map((menu)=><Menu key={menu._id} menu={menu}/>)
+                    menu && menu.slice(0,display).map((menu)=><Menu key={menu._id} menu={menu}/>)
                     }
                     
                 </div>
-                <div className="flex items-center gap-2 py-3 mt-0 md:mt-10 justify-center border-b-2 border-green-600 w-32 rounded-2xl mx-auto shadow-2xl cursor-pointer hover:w-40 hover:gap-x-5 transition-transform  hover:scale-y-100 hover:ease-in-out hover:duration-500">
-                        <button className="text-green-600 hover:scale-5-100">Show More</button>
-                       
-                        <FaArrowDownLong fill="green"  className="mt-1 hover:scale-y-100" />
+                {
+                   menu.length < display?  <p className="py-3 mt-2 md:mt-5 text-center text-green-600 font-semibold text-lg md:text-xl"> No Data Available </p> : <div className="flex items-center gap-2 py-3 mt-0 md:mt-10 justify-center border-b-2 border-green-600 w-32 rounded-2xl mx-auto shadow-2xl cursor-pointer hover:w-40 hover:gap-x-5 transition-transform  hover:scale-y-100 hover:ease-in-out hover:duration-500">
+                    <button onClick={()=>setDisplay(display+24)} className="text-green-600 hover:scale-5-100">Show More</button>
+                   
+                    <FaArrowDownLong fill="green"  className="mt-1 hover:scale-y-100" />
 
-                    </div>
+                </div> 
+                }
         </div>
     )
 };
