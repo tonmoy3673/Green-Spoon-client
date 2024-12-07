f00f7b0fcbb45433c1ccd27134f5ed4e87042f0e
tonmoy <tonmoy3673@gmail.com>
2024-12-08 00:09:01 +0600

    :tada: create chef section

--- src/Components/Pages/Home/Chef.jsx
+++ src/Components/Pages/Home/Chef.jsx
@@ -2,8 +2,16 @@ import './Chef.css';
 const Chef =()=>{
     return (
         <div className="py-8 md:py-16 bg">
-            <section>
+            <section className='p-6 md:p-8 text-center bg-slate-100 w-1/2 mx-auto pb-8'>
+                <h2 className='text-2xl text-black pb-3 md:pb-5'>Green Spoon</h2>
+                <p>Green Spoon Restaurant offers a delightful fusion of fresh, organic, and sustainably sourced ingredients to create vibrant, flavorful dishes. Experience exceptional dining in a cozy, eco-friendly atmosphere!
 
+
+
+
+
+
+</p>
             </section>
         </div>
     )
