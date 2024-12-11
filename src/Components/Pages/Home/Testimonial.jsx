8b4c5bf8bb36cdfcc458898487569cbfad864fd1
tonmoy <tonmoy3673@gmail.com>
2024-12-11 12:01:12 +0600

    :bulb: added swiper in testimonial

--- src/Components/Pages/Home/Testimonial.jsx
+++ src/Components/Pages/Home/Testimonial.jsx
@@ -1,7 +1,35 @@
+import Swiper from "swiper";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Mousewheel, Pagination } from "swiper/modules";
+import { SwiperSlide } from "swiper/react";
+
+import './styles.css';
 const Testimonial =()=>{
     return (
         <div>
-
+             <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
         </div>
     )
 };
